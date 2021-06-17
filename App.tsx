d64e70c7b8febdf04978a6fe6e5f0d143b1824d2
ru47fat <stael.tchinda@campus.lmu.de>
2021-06-17 17:55:00 +0200

    Init ui-kitten Made homepage

--- App.tsx
+++ App.tsx
@@ -1,21 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
+// import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as eva from '@eva-design/eva';
+import { StyleSheet, Image, View } from 'react-native';
+import { ApplicationProvider, Layout, Text } from '@ui-kitten/components';
+import { default as theme } from './assets/theme.json'; // <-- Import app theme
+
+
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+
+    <ApplicationProvider {...eva} theme={{ ...eva.dark, ...theme }}>
+      <Layout style={styles.container}>
+        <Layout style={styles.imageContainer}>
+          <Image
+            style={styles.logo}
+            source={ require('./assets/logo.png') }
+          />
+        </Layout>
+        <Text status='success' category='h1'>Green Home</Text>
+      </Layout>
+    </ApplicationProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  imageContainer: {
+    borderRadius: 200,
+    backgroundColor: '#fff',
+    padding: 30, 
+    marginBottom: 10
+  },
+  logo: {
+    width: 130,
+    height: 130,
+    resizeMode: 'center',
+  }
 });
